607817ab0f305f181175c2a11688c8d5fde9b261
Destiny Junior <destinyjuniorsdj@gmail.com>
2024-08-21 11:32:13 +0100

    feat: installed tailwind

---
+++ pages/auth/signin.vue
@@ -0,0 +1,7 @@
+<script>
+</script>
+<template>
+    <div>
+        <h3 class="text-purple-700 font-bold text-5xl">Signin Page</h3>
+    </div>
+</template>
---
+++ pages/dashboard/settings.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1 class="text-purple-700 font-bold text-5xl">This is the Settings</h1>
+  </div>
+</template>
