29a67bf1142a8a9a1334fa4c8f06557ca254db1e
best2025j <audugodstime95@gmail.com>
2024-08-27 14:00:19 -0700

    sidenav for message chat done

--- components/chats/SideNav.vue
+++ components/chats/SideNav.vue
@@ -1,7 +1,7 @@
 <template>
   <aside class="bg-[#FFFFFF] text-black-800 min-h-screen fixed w-[296px]">
     <h1 class="font-bold text-xl p-4">Messages</h1>
-    <div class="flex items-start justify-between p-4">
+    <div class="flex items-start justify-between p-4 border-b-2">
       <img src="/Ellipse 2.png" alt="no pix" />
       <div class="space-y-1">
         <h1 class="text-sm">Guy Hawkins</h1>
@@ -10,8 +10,8 @@
       <span class="text-xs">15min</span>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (3).png" alt="no pix" />
       <div class="space-y-1">
         <h1 class="text-sm">Jane Cooper</h1>
         <h1 class="text-sm">What’s the difference...</h1>
@@ -19,8 +19,8 @@
       <span class="text-xs">17min</span>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (4).png" alt="no pix" />
       <div class="space-y-1">
         <h1 class="text-sm font-bold">Dianne Russell</h1>
         <h1 class="text-sm font-bold">What’s the difference...</h1>
@@ -38,8 +38,8 @@
       </div>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (5).png" alt="no pix" />
       <div class="space-y-1">
         <h1 class="text-sm font-bold">Robert Fox</h1>
         <h1 class="text-sm font-bold">What’s the difference...</h1>
@@ -57,49 +57,49 @@
       </div>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (6).png" alt="no pix" />
       <div class="space-y-1">
-        <h1 class="text-sm">Guy Hawkins</h1>
+        <h1 class="text-sm">Savannah Nguyen</h1>
         <h1 class="text-sm">What’s the difference...</h1>
       </div>
-      <span class="text-xs">15min</span>
+      <span class="text-xs">12:04</span>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (7).png" alt="no pix" />
       <div class="space-y-1">
-        <h1 class="text-sm">Guy Hawkins</h1>
+        <h1 class="text-sm">Arlene McCoy</h1>
         <h1 class="text-sm">What’s the difference...</h1>
       </div>
-      <span class="text-xs">15min</span>
+      <span class="text-xs">00:34</span>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (8).png" alt="no pix" />
       <div class="space-y-1">
-        <h1 class="text-sm">Guy Hawkins</h1>
+        <h1 class="text-sm">Kathryn Murphy</h1>
         <h1 class="text-sm">What’s the difference...</h1>
       </div>
-      <span class="text-xs">15min</span>
+      <span class="text-xs">02:27</span>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (9).png" alt="no pix" />
       <div class="space-y-1">
-        <h1 class="text-sm">Guy Hawkins</h1>
+        <h1 class="text-sm">Floyd Miles</h1>
         <h1 class="text-sm">What’s the difference...</h1>
       </div>
-      <span class="text-xs">15min</span>
+      <span class="text-xs">07:25</span>
     </div>
     <!--  -->
-    <div class="flex items-start justify-between p-4">
-      <img src="/Ellipse 2.png" alt="no pix" />
+    <div class="flex items-start justify-between p-4 border-b-2">
+      <img src="/components/images/Ellipse 2 (10).png" alt="no pix" />
       <div class="space-y-1">
-        <h1 class="text-sm">Guy Hawkins</h1>
+        <h1 class="text-sm">Darlene Robertson</h1>
         <h1 class="text-sm">What’s the difference...</h1>
       </div>
-      <span class="text-xs">15min</span>
+      <span class="text-xs">07:33</span>
     </div>
   </aside>
 </template>
