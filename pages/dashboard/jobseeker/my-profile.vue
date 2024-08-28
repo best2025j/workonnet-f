c7f1383cec835c127090d3ed9a3c37ba6cd580e6
best2025j <audugodstime95@gmail.com>
2024-08-28 01:40:57 -0700

    profile page done successfully

--- pages/dashboard/jobseeker/my-profile.vue
+++ pages/dashboard/jobseeker/my-profile.vue
@@ -167,119 +167,186 @@ definePageMeta({
       </div>
     </div>
 
-    <!-- cards -->
-    <div class="p-4 bg-white rounded-10 w-[670px] my-4">
-      <div class="space-y-4 pt-4">
-        <h1 class="text-xl font-bold">About me</h1>
-        <p class="text-xs tracking-wider">
-          Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-          accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-          nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-          varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi nulla
-          sed sed proin pulvinar. Libero sed donec lorem blandit aliquam diam.
-          Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus ut
-          vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-          porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-          velit interdum fames pellentesque.
-        </p>
+    <div class="flex gap-4">
+      <!-- cards -->
+      <div class="p-4 bg-white rounded-10 w-full my-4">
+        <div class="space-y-4 pt-4">
+          <h1 class="text-xl font-bold">About me</h1>
+          <p class="text-xs tracking-wider">
+            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
+            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
+            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
+            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
+            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
+            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
+            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
+            velit interdum fames pellentesque.
+          </p>
+        </div>
+        <div class="space-y-4 pt-10">
+          <h1 class="text-xl font-bold">Experience</h1>
+          <div class="bg-westside-50 space-y-4 rounded-10 p-4">
+            <img src="/Logo2.png" alt="no image yet" />
+            <h1 class="text-info-600 font-bold text-sm">
+              User Interface Designer at Workonnect
+            </h1>
+            <div class="flex gap-2">
+              <h1 class="text-xs font-bold">May - June 2020</h1>
+              <h1 class="text-xs">Lagos, Nigeria.</h1>
+            </div>
+            <p class="text-xs tracking-wider">
+              Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+              accumsan eget nisl felis magna rhoncus. Nisl duis netus id
+              lobortis nec dui leo. Ornare scelerisque vivamus egestas
+              adipiscing in amet varius. Velit in nec dolor ultrices
+              scelerisque. Ipsum facilisi nulla sed sed proin pulvinar. Libero
+              sed donec lorem blandit aliquam diam. Egestas et amet elit a nulla
+              rhoncus sagittis in sem. Lectus ut vivamus id at vitae. Elit non
+              ut eget massa id lorem tincidunt porttitor. Viverra duis sit a
+              dignissim molestie placerat. Tempus velit interdum fames
+              pellentesque.
+            </p>
+            <p class="text-xs tracking-wider">
+              Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+              accumsan eget nisl felis magna rhoncus. Nisl duis netus id
+              lobortis nec dui leo. Ornare scelerisque vivamus egestas
+              adipiscing in amet varius. Velit in nec dolor ultrices
+              scelerisque. Ipsum facilisi nulla sed sed proin pulvinar. Libero
+              sed donec lorem blandit aliquam diam. Egestas et amet elit a nulla
+              rhoncus sagittis in sem. Lectus ut vivamus id at vitae. Elit non
+              ut eget massa id lorem tincidunt porttitor. Viverra duis sit a
+              dignissim molestie placerat. Tempus velit interdum fames
+              pellentesque.
+            </p>
+          </div>
+          <div class="bg-westside-50 space-y-4 rounded-10 p-4">
+            <img src="/ms.png" alt="no image yet" />
+            <h1 class="text-info-600 font-bold text-sm">
+              User Interface Designer at Workonnect
+            </h1>
+            <div class="flex gap-2">
+              <h1 class="text-xs font-bold">May - June 2020</h1>
+              <h1 class="text-xs">Lagos, Nigeria.</h1>
+            </div>
+            <p class="text-xs tracking-wider">
+              Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+              accumsan eget nisl felis magna rhoncus. Nisl duis netus id
+              lobortis nec dui leo. Ornare scelerisque vivamus egestas
+              adipiscing in amet varius. Velit in nec dolor ultrices
+              scelerisque. Ipsum facilisi nulla sed sed proin pulvinar. Libero
+              sed donec lorem blandit aliquam diam. Egestas et amet elit a nulla
+              rhoncus sagittis in sem. Lectus ut vivamus id at vitae. Elit non
+              ut eget massa id lorem tincidunt porttitor. Viverra duis sit a
+              dignissim molestie placerat. Tempus velit interdum fames
+              pellentesque.
+            </p>
+            <p class="text-xs tracking-wider">
+              Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+              accumsan eget nisl felis magna rhoncus. Nisl duis netus id
+              lobortis nec dui leo. Ornare scelerisque vivamus egestas
+              adipiscing in amet varius. Velit in nec dolor ultrices
+              scelerisque. Ipsum facilisi nulla sed sed proin pulvinar. Libero
+              sed donec lorem blandit aliquam diam. Egestas et amet elit a nulla
+              rhoncus sagittis in sem. Lectus ut vivamus id at vitae. Elit non
+              ut eget massa id lorem tincidunt porttitor. Viverra duis sit a
+              dignissim molestie placerat. Tempus velit interdum fames
+              pellentesque.
+            </p>
+          </div>
+          <div class="bg-westside-50 space-y-4 rounded-10 p-4">
+            <img src="/ms.png" alt="no image yet" />
+            <h1 class="text-info-600 font-bold text-sm">
+              User Interface Designer at Workonnect
+            </h1>
+            <div class="flex gap-2">
+              <h1 class="text-xs font-bold">May - June 2020</h1>
+              <h1 class="text-xs">Lagos, Nigeria.</h1>
+            </div>
+            <p class="text-xs tracking-wider">
+              Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+              accumsan eget nisl felis magna rhoncus. Nisl duis netus id
+              lobortis nec dui leo. Ornare scelerisque vivamus egestas
+              adipiscing in amet varius. Velit in nec dolor ultrices
+              scelerisque. Ipsum facilisi nulla sed sed proin pulvinar. Libero
+              sed donec lorem blandit aliquam diam. Egestas et amet elit a nulla
+              rhoncus sagittis in sem. Lectus ut vivamus id at vitae. Elit non
+              ut eget massa id lorem tincidunt porttitor. Viverra duis sit a
+              dignissim molestie placerat. Tempus velit interdum fames
+              pellentesque.
+            </p>
+            <p class="text-xs tracking-wider">
+              Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
+              accumsan eget nisl felis magna rhoncus. Nisl duis netus id
+              lobortis nec dui leo. Ornare scelerisque vivamus egestas
+              adipiscing in amet varius. Velit in nec dolor ultrices
+              scelerisque. Ipsum facilisi nulla sed sed proin pulvinar. Libero
+              sed donec lorem blandit aliquam diam. Egestas et amet elit a nulla
+              rhoncus sagittis in sem. Lectus ut vivamus id at vitae. Elit non
+              ut eget massa id lorem tincidunt porttitor. Viverra duis sit a
+              dignissim molestie placerat. Tempus velit interdum fames
+              pellentesque.
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="space-y-4 pt-10">
-        <h1 class="text-xl font-bold">Experience</h1>
-        <div class="bg-westside-50 space-y-4 rounded-10 p-4">
-          <img src="/Logo2.png" alt="no image yet" />
-          <h1 class="text-info-600 font-bold text-sm">
-            User Interface Designer at Workonnect
-          </h1>
-          <div class="flex gap-2">
-            <h1 class="text-xs font-bold">May - June 2020</h1>
-            <h1 class="text-xs">Lagos, Nigeria.</h1>
+
+      <!--  -->
+      <div class="p-4 bg-white rounded-10 my-4 w-[670px] h-full">
+        <div class="space-y-6">
+          <h1 class="text-xl font-bold">Uploaded Documents</h1>
+          <div class="flex justify-between">
+            <div class="flex items-start gap-x-2">
+              <svg
+                width="18"
+                height="20"
+                viewBox="0 0 18 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.0306 5.71938L11.7806 0.469375C11.7109 0.399749 11.6282 0.344539 11.5371 0.306898C11.4461 0.269257 11.3485 0.249923 11.25 0.25H2.25C1.85218 0.25 1.47064 0.408035 1.18934 0.68934C0.908035 0.970645 0.75 1.35218 0.75 1.75V18.25C0.75 18.6478 0.908035 19.0294 1.18934 19.3107C1.47064 19.592 1.85218 19.75 2.25 19.75H15.75C16.1478 19.75 16.5294 19.592 16.8107 19.3107C17.092 19.0294 17.25 18.6478 17.25 18.25V6.25C17.2501 6.15148 17.2307 6.05391 17.1931 5.96286C17.1555 5.87182 17.1003 5.78908 17.0306 5.71938ZM12 2.81031L14.6897 5.5H12V2.81031ZM15.75 18.25H2.25V1.75H10.5V6.25C10.5 6.44891 10.579 6.63968 10.7197 6.78033C10.8603 6.92098 11.0511 7 11.25 7H15.75V18.25Z"
+                  fill="#343330"
+                />
+              </svg>
+              <div>
+                <h1 class="text-xs">Resume/CV</h1>
+                <p class="text-xs text-info-600">Document Name.pdf</p>
+              </div>
+            </div>
+
+            <button
+              class="px-[14px] text-xs py-2 text-primary-1 rounded-10 border border-primary-1"
+            >
+              Change
+            </button>
           </div>
-          <p class="text-xs tracking-wider">
-            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
-            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
-            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
-            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-            velit interdum fames pellentesque.
-          </p>
-          <p class="text-xs tracking-wider">
-            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
-            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
-            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
-            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-            velit interdum fames pellentesque.
-          </p>
-        </div>
-        <div class="bg-westside-50 space-y-4 rounded-10 p-4">
-          <img src="/ms.png" alt="no image yet" />
-          <h1 class="text-info-600 font-bold text-sm">
-            User Interface Designer at Workonnect
-          </h1>
-          <div class="flex gap-2">
-            <h1 class="text-xs font-bold">May - June 2020</h1>
-            <h1 class="text-xs">Lagos, Nigeria.</h1>
+          <div class="flex justify-between">
+            <div class="flex items-start gap-x-2">
+              <svg
+                width="18"
+                height="20"
+                viewBox="0 0 18 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.0306 5.71938L11.7806 0.469375C11.7109 0.399749 11.6282 0.344539 11.5371 0.306898C11.4461 0.269257 11.3485 0.249923 11.25 0.25H2.25C1.85218 0.25 1.47064 0.408035 1.18934 0.68934C0.908035 0.970645 0.75 1.35218 0.75 1.75V18.25C0.75 18.6478 0.908035 19.0294 1.18934 19.3107C1.47064 19.592 1.85218 19.75 2.25 19.75H15.75C16.1478 19.75 16.5294 19.592 16.8107 19.3107C17.092 19.0294 17.25 18.6478 17.25 18.25V6.25C17.2501 6.15148 17.2307 6.05391 17.1931 5.96286C17.1555 5.87182 17.1003 5.78908 17.0306 5.71938ZM12 2.81031L14.6897 5.5H12V2.81031ZM15.75 18.25H2.25V1.75H10.5V6.25C10.5 6.44891 10.579 6.63968 10.7197 6.78033C10.8603 6.92098 11.0511 7 11.25 7H15.75V18.25Z"
+                  fill="#343330"
+                />
+              </svg>
+              <div>
+                <h1 class="text-xs">Resume/CV</h1>
+                <p class="text-xs text-info-600">Document Name.pdf</p>
+              </div>
+            </div>
+
+            <button
+              class="px-[14px] text-xs py-2 text-primary-1 rounded-10 border border-primary-1"
+            >
+              Change
+            </button>
           </div>
-          <p class="text-xs tracking-wider">
-            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
-            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
-            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
-            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-            velit interdum fames pellentesque.
-          </p>
-          <p class="text-xs tracking-wider">
-            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
-            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
-            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
-            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-            velit interdum fames pellentesque.
-          </p>
-        </div>
-        <div class="bg-westside-50 space-y-4 rounded-10 p-4">
-          <img src="/ms.png" alt="no image yet" />
-          <h1 class="text-info-600 font-bold text-sm">
-            User Interface Designer at Workonnect
-          </h1>
-          <div class="flex gap-2">
-            <h1 class="text-xs font-bold">May - June 2020</h1>
-            <h1 class="text-xs">Lagos, Nigeria.</h1>
-          </div>
-          <p class="text-xs tracking-wider">
-            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
-            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
-            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
-            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-            velit interdum fames pellentesque.
-          </p>
-          <p class="text-xs tracking-wider">
-            Lorem ipsum dolor sit amet consectetur. Dignissim aliquam vitae
-            accumsan eget nisl felis magna rhoncus. Nisl duis netus id lobortis
-            nec dui leo. Ornare scelerisque vivamus egestas adipiscing in amet
-            varius. Velit in nec dolor ultrices scelerisque. Ipsum facilisi
-            nulla sed sed proin pulvinar. Libero sed donec lorem blandit aliquam
-            diam. Egestas et amet elit a nulla rhoncus sagittis in sem. Lectus
-            ut vivamus id at vitae. Elit non ut eget massa id lorem tincidunt
-            porttitor. Viverra duis sit a dignissim molestie placerat. Tempus
-            velit interdum fames pellentesque.
-          </p>
         </div>
       </div>
     </div>
