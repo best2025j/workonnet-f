b62a4e40bc4ab6e1f8e303a4daf9a81cceabe98b
best2025j <audugodstime95@gmail.com>
2024-09-07 18:22:37 +0100

    changes: made

---
+++ pages/admin/dashboard/home/posted-job.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  title: "Subscriptions",
+  pageName: "admin.dashboard.home.posted-jobs",
+  layout: "dashboard",
+});
+</script>
+
+
+
+<template>
+  
+</template>
