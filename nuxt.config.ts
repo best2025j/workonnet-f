4fd6ce65ee8f0d6a8f3eeee967e3142d2e304206
best2025j <audugodstime95@gmail.com>
2024-09-09 22:08:20 +0100

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@
 import { defineNuxtConfig } from "nuxt/config";
 // import { defineNuxtConfig } from 'nuxt3';
 
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
@@ -12,10 +13,5 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
-  },
-  plugins: [{ src: '~/plugins/scroll-behavior.client.ts', mode: 'client' }],
-  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
-  pinia: {
-    storesDirs: ['./store/**'],
   },
 });
