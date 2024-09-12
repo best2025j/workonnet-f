afec192cbb4d6f477b39f4da064abd13d580a8a8
Boniface Audu <audugodstime95@gmail.com>
2024-09-12 11:28:12 +0100

    layout added to landing page

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <LandingPageLayout>
+      <div class="h-screen w-full py-14">About page</div>
+    </LandingPageLayout>
+  </div>
+</template>
