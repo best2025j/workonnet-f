0570c21c64f97e98927b505335607007268a3286
Boniface Audu <audugodstime95@gmail.com>
2024-11-08 21:42:54 +0100

    changes made

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <script>
 import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
+import "aos/dist/aos.css";
+
 AOS.init();
 AOS.refresh();
+
+if (process.client) {
+  import("aos").then((AOS) => {
+    AOS.init();
+  });
+}
 </script>
 <template>
   <!-- <NuxtRouteAnnouncer /> -->
